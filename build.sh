44b1edc339ed599ac3d04842f6b053e49a4557b6
Bob Kozdemba <bkozdemba@gmail.com>
2018-12-06 10:23:10 -0600

    Added gpu

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 # https://www.server-world.info/en/note?os=CentOS_7&p=tensorflow&f=1
 #
 docker build --rm=true --force-rm=true --tag koz/ml .
+
+# docker build --rm=true --force-rm=true --tag koz/ml-gpu --file=Dockerfile=gpu .
